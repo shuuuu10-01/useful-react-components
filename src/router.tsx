c9a55a000ceb7866bb95deb0bdeec42292b284e7
shuuuu10-01 <koron.konan123@gmail.com>
2023-03-23 00:25:34 +0900

    add: react-router-domの追加と不要なファイルの削除

---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ButtonPage from "./pages/Button/ButtonPage";
+
+const Router: FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/buttons" element={<ButtonPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
