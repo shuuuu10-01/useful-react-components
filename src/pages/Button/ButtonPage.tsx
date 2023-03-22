bde1155fdfcd3d1b9201699e59ba7bdc296a75f7
shuuuu10-01 <koron.konan123@gmail.com>
2023-03-23 00:38:31 +0900

    add: ボタンのサンプルページの追加

---
+++ src/pages/Button/ButtonPage.tsx
@@ -0,0 +1,48 @@
+import { FC } from "react";
+import Button from "../../components/Button/Button";
+import styles from "./ButtonPage.module.css";
+import { FaSistrix, FaInfo, FaTrash } from "react-icons/fa";
+
+const ButtonPage: FC = () => {
+  return (
+    <div className={styles.pageWrapper}>
+      <h4 className={styles.title}>size: medium</h4>
+      <div className={styles.buttonWrapper}>
+        <Button visual="primary">primary</Button>
+        <Button visual="secondary">secondary</Button>
+        <Button visual="alert">alert</Button>
+      </div>
+
+      <h4 className={styles.title}>custom</h4>
+      <div className={styles.buttonWrapper}>
+        <Button visual="primary" size="small">
+          primary & small
+        </Button>
+        <Button visual="secondary" size="large">
+          secondary & large
+        </Button>
+        <Button visual="alert" size="medium">
+          alert & medium
+        </Button>
+      </div>
+
+      <h4 className={styles.title}>has icon</h4>
+      <div className={styles.buttonWrapper}>
+        <Button visual="primary" className={styles.hasIcon}>
+          <FaSistrix />
+          検索
+        </Button>
+        <Button visual="secondary" className={styles.hasIcon}>
+          <FaInfo />
+          お知らせ
+        </Button>
+        <Button visual="alert" className={styles.hasIcon}>
+          <FaTrash />
+          削除します
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonPage;
