6ceaf2d344b473c47881c603a40ce4216db75db0
shuuuu10-01 <koron.konan123@gmail.com>
2023-03-23 22:43:55 +0900

    add: Galleryの追加

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,5 +1,7 @@
+import styles from "./Button.module.css";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Button from "./Button";
+import { FaSistrix, FaInfo, FaTrash } from "react-icons/fa";
 
 export default {
   title: "Button",
@@ -18,3 +20,45 @@ SecondaryButton.args = { visual: "secondary", size: "medium", disabled: false };
 
 export const AlertButton = Template.bind({});
 AlertButton.args = { visual: "alert", size: "medium", disabled: false };
+
+export const Gallery = () => {
+  return (
+    <>
+      <h4 className={styles.title}>size: medium</h4>
+      <div className={styles.buttonWrapper}>
+        <Button visual="primary">primary</Button>
+        <Button visual="secondary">secondary</Button>
+        <Button visual="alert">alert</Button>
+      </div>
+
+      <h4 className={styles.title}>disabled</h4>
+      <div className={styles.buttonWrapper}>
+        <Button visual="primary" size="large" disabled>
+          small
+        </Button>
+        <Button visual="secondary" size="medium" disabled>
+          large
+        </Button>
+        <Button visual="alert" size="small" disabled>
+          medium
+        </Button>
+      </div>
+
+      <h4 className={styles.title}>アイコン有り</h4>
+      <div className={styles.buttonWrapper}>
+        <Button visual="primary" className={styles.hasIcon}>
+          <FaSistrix />
+          検索
+        </Button>
+        <Button visual="secondary" className={styles.hasIcon}>
+          <FaInfo />
+          お知らせ
+        </Button>
+        <Button visual="alert" className={styles.hasIcon}>
+          <FaTrash />
+          削除します
+        </Button>
+      </div>
+    </>
+  );
+};
