20ca10303d500c30054200a2f86b06c449b6a4c9
shuuuu10-01 <koron.konan123@gmail.com>
2023-05-14 21:39:00 +0900

    add: storybookの追加

---
+++ src/components/Tab/Tab.stories.tsx
@@ -0,0 +1,67 @@
+import { ComponentMeta } from "@storybook/react";
+import Tab from "./Tab";
+import TabItem from "./TabItem";
+import { FaInfo } from "react-icons/fa";
+import styles from "./Tab.module.css";
+
+export default {
+  title: "Tab",
+} as ComponentMeta<typeof Tab>;
+
+export const Text = () => {
+  return (
+    <div style={{ width: "500px" }}>
+      <Tab defaultKey="a">
+        <TabItem tabKey="a" label="a">
+          aaaaaaaaaa
+        </TabItem>
+        <TabItem tabKey="b" label="b">
+          bbbbbbbbbb
+        </TabItem>
+      </Tab>
+    </div>
+  );
+};
+
+export const Number = () => {
+  return (
+    <div style={{ width: "500px" }}>
+      <Tab defaultKey={1}>
+        {[1, 2, 3, 4, 5].map((num) => {
+          return (
+            <TabItem tabKey={num} label={num} key={num}>
+              {num}
+            </TabItem>
+          );
+        })}
+      </Tab>
+    </div>
+  );
+};
+
+export const Icon = () => {
+  const headerLabel = (label: string) => {
+    return (
+      <div className={styles.hasIcon}>
+        <FaInfo />
+        {label}
+      </div>
+    );
+  };
+
+  return (
+    <div style={{ width: "500px" }}>
+      <Tab defaultKey="1st">
+        <TabItem tabKey="1st" label={headerLabel("1st")}>
+          1st content
+        </TabItem>
+        <TabItem tabKey="2nd" label={headerLabel("2nd")}>
+          2nd content
+        </TabItem>
+        <TabItem tabKey="3rd" label={headerLabel("3rd")}>
+          3rd content
+        </TabItem>
+      </Tab>
+    </div>
+  );
+};
