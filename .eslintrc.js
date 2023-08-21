7ddac2d0bfd8584ed124274c13784ca2205b94d8
shuuuu10-01 <koron.konan123@gmail.com>
2023-08-21 21:23:27 +0900

    add: eslint-plugin-importとeslint-plugin-unused-importsを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,11 +26,44 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint", "react"],
+  plugins: ["@typescript-eslint", "react", "import", "unused-imports"],
   rules: {
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "warn",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: { order: "asc", caseInsensitive: true },
+        pathGroups: [
+          // ここに書いた順序で間に1行空行をあけつつ整頓される
+          { pattern: "src/types/**", group: "internal", position: "before" },
+          {
+            pattern: "src/repositories/**",
+            group: "internal",
+            position: "before",
+          },
+          // TODO：ここはディレクトリ構成が決まり次第設定
+          // styles
+          // 最後尾にしたいのでgroupをindex扱いにする
+          { pattern: "./**.module.css", group: "index", position: "before" },
+        ],
+      },
+    ],
   },
   settings: {
     react: {
