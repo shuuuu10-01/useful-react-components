20541c17b59f6894ce5d00945cdd93886ec13d7e
shuuuu10-01 <koron.konan123@gmail.com>
2023-12-26 23:22:04 +0900

    refactor: 不要な記述の削除と記述順の変更

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -26,20 +26,17 @@ type Props = ComponentPropsWithoutRef<"select"> & {
 const Select = forwardRef<HTMLSelectElement, Props>(
   ({ options, upper = false, onSelect, defaultValue = "", ...props }, ref) => {
     const [selected, setSelected] = useState<string>(defaultValue);
-
     const [isOpen, setIsOpen] = useState(false);
+    const optionsRef = useRef<HTMLDivElement>(null);
 
     const selectedLabel = useMemo(() => {
       const selectItem = options.find(({ value }) => value === selected);
-      if (selectItem === undefined) {
-        return "";
+      if (selectItem) {
+        return selectItem.label ?? selectItem.value;
       } else {
-        return selectItem?.label || selectItem.value;
+        return "";
       }
     }, [options, selected]);
-
-    const optionsRef = useRef<HTMLDivElement>(null);
-    const toggleRef = useRef<HTMLButtonElement>(null);
 
     const handleToggle = () => {
       setIsOpen((prev) => !prev);
@@ -48,44 +45,23 @@ const Select = forwardRef<HTMLSelectElement, Props>(
     const handleBlur = (event: FocusEvent<HTMLButtonElement>) => {
       const relatedTarget = event.relatedTarget as HTMLElement;
       if (optionsRef.current?.contains(relatedTarget)) return;
-
       setIsOpen(false);
     };
 
     const handleSelect = (value: string) => {
       setSelected(value);
-      // 親に変更を伝播
       onSelect?.(value);
-      // 選択後、選択肢を閉じない場合は以下のコメントアウトを外す
-      // toggleRef.current?.focus();
-      // 選択後、選択肢を閉じる場合は以下のコメントアウトを外す
       setIsOpen(false);
     };
 
     const optionsHeight = useMemo(() => {
-      // isOpen ? 選択肢の数 x 選択肢の高さ + トグルボタンの高さ : トグルボタンの高さ
+      // isOpen ? 選択肢の数 * 選択肢の高さ + トグルボタンの高さ : トグルボタンの高さ
       return isOpen ? `${options.length * 40 + 40}px` : "40px";
     }, [isOpen]);
 
     return (
       <div className={styles.wrapper}>
-        <select
-          ref={ref}
-          hidden
-          value={selected}
-          onChange={(e) => setSelected(e.target.value)}
-          {...props}
-        >
-          {options.map(({ value, label = value }) => {
-            return (
-              <option key={value} value={value}>
-                {label}
-              </option>
-            );
-          })}
-        </select>
         <button
-          ref={toggleRef}
           className={styles.button}
           onClick={handleToggle}
           onBlur={handleBlur}
@@ -105,7 +81,6 @@ const Select = forwardRef<HTMLSelectElement, Props>(
           )}
           style={{ height: optionsHeight }}
         >
-          {isOpen && !upper && <div className={styles.lastOption} />}
           {options.map(({ value, label = value }) => {
             return (
               <button
@@ -118,8 +93,22 @@ const Select = forwardRef<HTMLSelectElement, Props>(
               </button>
             );
           })}
-          {isOpen && upper && <div className={styles.lastItem} />}
         </div>
+        <select
+          ref={ref}
+          hidden
+          value={selected}
+          onChange={(e) => setSelected(e.target.value)}
+          {...props}
+        >
+          {options.map(({ value, label = value }) => {
+            return (
+              <option key={value} value={value}>
+                {label}
+              </option>
+            );
+          })}
+        </select>
       </div>
     );
   }
